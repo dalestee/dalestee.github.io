1773c0e284dba6ab4f98f25ae49df03223f38481
Caetano Godinat <caetano.godinat@hotmail.com>
2023-09-25 14:44:01 +0200

    working point following the mouse

--- test/script.js
+++ test/script.js
@@ -3,6 +3,6 @@ var dot = document.getElementById("dot");
 document.addEventListener("mousemove", function(event) {
   var x = event.clientX;
   var y = event.clientY;
-  dot.style.left = x + 10 + "px";
-  dot.style.top = y + 10 + "px";
+  dot.style.left = x +10 + "px";
+  dot.style.top = y -+10 + "px";
 });
