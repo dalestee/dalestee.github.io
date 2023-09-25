9919a784c2437cbb2213b7d7d1a9879455fa4216
Caetano Godinat <caetano.godinat@hotmail.com>
2023-09-25 15:35:11 +0200

    function on initDot

--- scritpt.js
+++ scritpt.js
@@ -1,8 +1,10 @@
-
-var dot = document.getElementById("dot");
-document.addEventListener("mousemove", function(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-  dot.style.left = x + 10 + "px";
-  dot.style.top = y + 10 + "px";
+function dotInit(){
+  var dot = document.getElementById("dot");
+  document.addEventListener("mousemove", function(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    dot.style.left = x + 10 + "px";
+    dot.style.top = y + 10 + "px";
 });
+}
+dotInit()
