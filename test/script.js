1979438281b110d5ef69dba357fbcf7c7ededb21
Caetano Godinat <caetano.godinat@hotmail.com>
2023-09-26 10:24:01 +0200

    bobSleep

--- test/script.js
+++ test/script.js
@@ -1,8 +1,13 @@
 
 var dot = document.getElementById("dot");
+var offset = 0
+var vel = 0
 document.addEventListener("mousemove", function(event) {
   var x = event.clientX;
   var y = event.clientY;
   dot.style.left = x +10 + "px";
-  dot.style.top = y -+10 + "px";
+  dot.style.top = y -offset + "px";
+  offset+=inc
+  if (offset > 10)inc = -1
+  if (offset < -10)inc = 1
 });
