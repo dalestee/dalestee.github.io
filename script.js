3748b7ad61f06869b304beb18ccecaca6058e924
Caetano Godinat <caetano.godinat@hotmail.com>
2023-10-08 20:12:17 +0200

    settings are working :)

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let particles = [];
-const numberParticles = 200;
+let numberParticles = 200;
 
-const noiseScale = 0.001;
+let noiseScale = 0.001;
 
-const speedX = 1;
-const speedY = 1;
+let speedX = 1;
+let speedY = 1;
 
-const rotationScale = 150;
+let rotationScale = 150;
 
-const opacity = 10;
+let opacity = 10;
 
-const color = [125,255,155];
+let color = [125,255,155];
 
 function setup() {
   createCanvas(800, 400);
@@ -41,4 +41,30 @@ function onScreen(v) {
   return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
 }
 
+function applyChanges() {
+  clear()
+  let numberParticlesInput = document.getElementById("particlesChoice");
+  let noiseScaleInput = document.getElementById("noiseScaleChoice");
+  let rotationScaleInput = document.getElementById("rotationChoice");
+  let speedXInput = document.getElementById("speedXChoice");
+  let speedYInput = document.getElementById("speedYChoice");
+  let opacityInput = document.getElementById("opacityChoice");
+  let colorInput1 = document.getElementById("redChoice");
+  let colorInput2 = document.getElementById("greenChoice");
+  let colorInput3 = document.getElementById("blueChoice");
+
+  numberParticles = numberParticlesInput.value || 200;
+  noiseScale = noiseScaleInput.value || 0.001;
+  speedX = speedXInput.value || 1;
+  speedY = speedYInput.value || 1;
+  rotationScale = rotationScaleInput.value || 30;
+  opacity = opacityInput.value || 10;
+  color = [colorInput1.value || 0, colorInput2.value || 0, colorInput3.value || 0];
+  if (color[0] < 1 && color[1] < 1 && color[2] < 1) {
+    color = [0, 255, 0];
+  }
+  console.log(numberParticles, noiseScale, speedX, speedY, rotationScale, opacity, color)
+  noiseSeed(millis());
+  setup()
+}
 
